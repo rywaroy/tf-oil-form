7dd2dd1a196a574c3fc267d64fa62eb6d39acee3
张志豪 <464485682@qq.com>
2019-10-18 16:08:41 +0800

    feat: 弹窗组件

---
+++ src/pages/components/setColumn.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import { Table, Button, Modal, InputNumber, Icon, Input, Form, Radio } from 'antd';
+
+const alignOptions = [
+    { label: 'left', value: 'left' },
+    { label: 'right', value: 'right' },
+    { label: 'center', value: 'center' },
+]
+
+const ellipsisOptions = [
+    { label: 'false', value: 'false' },
+    { label: 'true', value: 'true' },
+]
+
+class SetColumn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+
+    /**
+     * 配置列
+     */
+    setLine() {
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log(values);
+            }
+        });
+    }
+
+    /**
+     * 关闭配置列弹窗
+     */
+    closeSetLine() {
+        this.setState({
+            visibleSet: false,
+        })
+    }
+
+    render() {
+        const formItemLayout = {
+            labelCol: { span: 6 },
+            wrapperCol: { span: 14 },
+        };
+        
+        const { getFieldDecorator } = this.props.form;
+
+        return (
+            <Modal
+                title="设置"
+                visible={this.props.visibleSet}
+                onOk={this.setLine.bind(this)}
+                onCancel={this.closeSetLine.bind(this)}>
+                    <Form  {...formItemLayout}>
+                        <Form.Item
+                            label="width">
+                                {getFieldDecorator('width')(
+                                    <InputNumber />
+                                )}
+                        </Form.Item>
+                        <Form.Item
+                            label="align">
+                                {getFieldDecorator('align')(
+                                    <Radio.Group options={alignOptions} />
+                                )}
+                        </Form.Item>
+                        <Form.Item
+                            label="ellipsis">
+                                {getFieldDecorator('ellipsis')(
+                                    <Radio.Group options={ellipsisOptions} />
+                                )}
+                        </Form.Item>
+                        <Form.Item
+                            label="className">
+                                {getFieldDecorator('className')(
+                                    <Input />
+                                )}
+                        </Form.Item>
+                    </Form>
+            </Modal>
+        );
+    }
+}
+
+const SetColumnForm = Form.create({ name: 'horizontal_login' })(SetColumn);
+ 
+export default SetColumnForm;
